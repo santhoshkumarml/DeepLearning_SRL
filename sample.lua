e3ca72ba646fe9b95c0d0e3f5ead3d6115017616
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2015-10-17 15:09:01 -0400

    Changes

--- sample.lua
+++ sample.lua
@@ -1,23 +1,34 @@
 require 'torch';
-
+require 'nn';
 
 START = "$START$"
 FINISH = "$END$"
 FILE_PATH = "dataset/language_model.data"
+WORD_VEC_SIZE = 25
+WINDOW_SIZE = 5
+
+-- initialize Random Word Vec
+function initWordVec()
+    return torch.randn(WORD_VEC_SIZE)
+end
 
 -- read and Store Data
 function readFile(file_path)
     local f = io.open(file_path)
-    store = {}
-    word_dict = {}
+    local store = {}
+    local word_dict = {}
+    word_dict[START] = initWordVec()
+    word_dict[FINISH] = initWordVec()
     while true do
         local l = f:read()
         if not l then break end
-        words = {}
+        local words = {}
         table.insert(words, START)
         for word in l:gmatch("%w+") do 
-            table.insert(words, word) 
-            word_dict[word] = true
+            table.insert(words, word)
+            if not word_dict[word] then
+                word_dict[word] = initWordVec()
+            end
         end
         table.insert(words,FINISH)
         table.insert(store, words)
@@ -26,53 +37,59 @@ function readFile(file_path)
 end
 
 -- split into windows and make training data
-function makeTrainingData(store, window_size)
-    train_data = {}
+function makeTrainingData(store, window_size, word_dict)
+    local train_data = {}
     for idx, line in ipairs(store) do
-        size = table.getn(line)
-        no_of_windows = math.max( 1, size - window_size)
+        local size = table.getn(line)
+        local no_of_windows = math.max( 0, size - window_size) + 1
 
         for widx = 1, no_of_windows do
+            local training_inst = {}
+            local data = {}
+            local word_idx = widx
 
-            training_inst = {}
-            data = {}
-
-            word_idx = widx+1
-
+            --Add to data the words for the window
             while table.getn(data) < window_size and word_idx < size do
                 table.insert(data, line[word_idx])
                 word_idx = word_idx + 1
             end
-            
+                
+            --Align to Window Size
             while word_idx >= size and table.getn(data) < window_size do
                 table.insert(data, FINISH)
                 word_idx = word_idx + 1
             end
+            -- Get WordVector From Dictionary
+            local word_vec_data = word_dict[data[1]]
+
+            for i = 2, table.getn(data) do
+                word_vec_data = torch.cat(word_vec_data, word_dict[data[i]])
+            end
             
-            training_inst.data = data
-            training_inst.label = '1'
+            training_inst.data = word_vec_data
+            training_inst.label = 1
             table.insert(train_data, training_inst)
         end   
     end
     return train_data
 end
    
-function construct_nn(windows_size, word_vec_size, hidden_layer_nodes)
+function construct_nn(window_size, word_vec_size, hidden_layer_nodes)
     -- Add NN Layers
-    net = nn.Sequential()
+    local net = nn.Sequential()
     net:add(nn.Linear(window_size * word_vec_size, hidden_layer_nodes))
     net:add(nn.Sigmoid())
     net:add(nn.Linear(hidden_layer_nodes, 1))
     net:add(nn.LogSoftMax())
     -- Define Loss Function
-    criterion = nn.ClassNLLCriterion()
+    local criterion = nn.ClassNLLCriterion()
     return net, criterion
 end
 
 function trainAndUpdatedWordVec(net, criterion, input, output)
-    for i = 1, 20 do
+    for i = 1, 5 do
         -- feed it to the neural network and the criterion
-        criterion:forward(net:forward(input), real_output)
+        criterion:forward(net:forward(input), output)
 
         -- train over this example in 3 steps
         -- (1) zero the accumulation of the gradients
@@ -81,12 +98,18 @@ function trainAndUpdatedWordVec(net, criterion, input, output)
         -- (2) accumulate gradients
         net:backward(input, criterion:backward(net.output, output))
         
-        grad_ip = net.gradInput
+        print(net.gradInput)
         
-        print(grad_ip)
-                
         -- (3) update parameters with a 0.01 learning rate
         net:updateParameters(0.01)
     end
     return data
+end
+
+store, word_dict = readFile(FILE_PATH)
+train_data = makeTrainingData(store, WINDOW_SIZE, word_dict)
+net, crit = construct_nn(WINDOW_SIZE, WORD_VEC_SIZE, 50)
+for idx, ins in ipairs(train_data) do
+    data = trainAndUpdatedWordVec(net, crit, ins.data, ins.label)
+    os.exit()
 end
