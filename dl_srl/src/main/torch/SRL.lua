56c643808bbc19e1571f24b1ee0065aab2ad0035
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2015-11-07 18:42:23 -0500

    Small Change

--- dl_srl/src/main/torch/SRL.lua
+++ dl_srl/src/main/torch/SRL.lua
@@ -4,16 +4,19 @@
 --
 require 'Constants';
 
-function get_nn_for_srl()
+function get_nn_for_srl(sentence_size)
      -- Add NN Layers
     local net = nn.Sequential()
 
-    local inputs = WINDOW_SIZE * WORD_VEC_SIZE;
+    local inputs = sentence_size * WORD_VEC_SIZE;
     local outputs = 2;
-    local HUs = 50;
+    local HUs = 100;
+    local conVHUs = 100;
+    local reduced_l = 3;
 
-    net:add(nn.Linear(inputs, HUs))
-    net:add(nn.Sigmoid())
+    net:add(nn.TemporalConvolution(inputs, conVHUs))
+    net:add(nn.TemporalMaxPooling(reduced_l))
+    net:add(nn.Tanh())
     net:add(nn.Linear(HUs, outputs))
     net:add(nn.LogSoftMax())
     return net
